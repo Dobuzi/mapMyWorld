8cac7a66f978cb44f140f75c076699a678f4424d
Dobuzi <kimjw7981@gmail.com>
2021-04-07 05:12:43 +0000

    submit 1

--- src/ball_chaser/src/process_image.cpp
+++ src/ball_chaser/src/process_image.cpp
@@ -5,7 +5,7 @@
 ros::ServiceClient client;
 
 void drive_robot(float lin_x, float ang_z) {
-  ROS_INFO_STREAM("Moving the arm to the center");
+  ROS_INFO_STREAM("Moving...");
 
   ball_chaser::DriveToTarget srv;
   srv.request.linear_x = lin_x;
@@ -23,33 +23,53 @@ void process_image_callback(const sensor_msgs::Image img) {
   int right = 0;
   float lin_x = 0.0;
   float ang_z = 0.0;
+  int max_zone = 0;
+  int max_threshold = 100000;
+  int min_threshold = 100;
+  float turn_left = -0.3;
+  float turn_right = 0.3;
+  float run_forward = 0.3;
 
   for (int i = 0; i < img.height * img.step; i++) {
     if (img.data[i] == white_pixel) {
       int position = i % img.step;
-      if (position < (img.step / 4)) {
+      if (position < (img.step / 3)) {
         left++;
-      } else if (position < (img.step * 3 / 4)) {
+      } else if (position < (img.step * 2 / 3)) {
         forward++;
       } else {
         right++;
       }
     }
   }
+
   if (left > forward) {
     if (left >  right) {
-      ang_z = -0.3;
+      max_zone = left;
+      ang_z = turn_left;
     } else {
-      ang_z = 0.3;
+      max_zone = right;
+      ang_z = turn_right;
     }
   } else {
     if (forward > right) {
-      lin_x = 0.3;
+      max_zone = forward;
+      lin_x = run_forward;
     } else {
-      ang_z = 0.3;
+      max_zone = right;
+      ang_z = turn_right;
     }
   }
-  drive_robot(lin_x, ang_z);
+
+  ROS_INFO_STREAM("left: " + std::to_string(left) + ", forward:" + std::to_string(forward) + ", right: " + std::to_string(right));
+  
+  if (max_zone > min_threshold) {
+    if (max_zone > max_threshold) {
+      lin_x = 0.0;
+      ang_z = 0.0;
+    }
+    drive_robot(lin_x, ang_z);
+  }
 }
 
 int main(int argc, char** argv) {
