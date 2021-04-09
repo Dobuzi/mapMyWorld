cee0043d8b39c96a36168a6cd845bf6703c50dfa
Dobuzi <kimjw7981@me.com>
2021-04-09 06:21:51 +0000

    submit 3

--- src/ball_chaser/src/process_image.cpp
+++ src/ball_chaser/src/process_image.cpp
@@ -5,8 +5,6 @@
 ros::ServiceClient client;
 
 void drive_robot(float lin_x, float ang_z) {
-  ROS_INFO_STREAM("Moving...");
-
   ball_chaser::DriveToTarget srv;
   srv.request.linear_x = lin_x;
   srv.request.angular_z = ang_z;
@@ -16,10 +14,7 @@ void drive_robot(float lin_x, float ang_z) {
   }
 }
 
-bool isWhite(const sensor_msgs::Image img, int i) {
-  int R = img.data[i];
-  int G = img.data[i+1];
-  int B = img.data[i+2];
+bool isWhite(int R, int G, int B) {
   if ((R == 255) && (G == 255) && (B == 255)) {
     return true;
   }
@@ -34,14 +29,18 @@ void process_image_callback(const sensor_msgs::Image img) {
   float lin_x = 0.0;
   float ang_z = 0.0;
   int max_zone = 0;
-  int max_threshold = 10000;
-  int min_threshold = 5;
-  float turn_left = -0.3;
-  float turn_right = 0.3;
+  int max_threshold = 50000;
+  int min_threshold = 500;
+  float turn_left = 0.3;
+  float turn_right = -0.3;
   float run_forward = 0.3;
+  int R, G, B;
 
   for (int i = 0; i < img.height * img.step; i++) {
-    if ((i%3 == 0) and isWhite(img, i)) {
+    R = img.data[i];
+    G = img.data[i+1];
+    B = img.data[i+2];
+    if ((i%3 == 0) and isWhite(R, G, B)) {
       int position = i % img.step;
       if (position < (img.step / 4)) {
         left++;
@@ -64,15 +63,13 @@ void process_image_callback(const sensor_msgs::Image img) {
     ang_z = turn_right;
   }
 
-  ROS_INFO_STREAM("left: " + std::to_string(left) + ", forward:" + std::to_string(forward) + ", right: " + std::to_string(right));
+  ROS_INFO_STREAM("left: " << std::to_string(left) << ", forward:" << std::to_string(forward) << ", right: " << std::to_string(right));
   
-  if (max_zone > min_threshold) {
-    if (max_zone > max_threshold) {
-      lin_x = 0.0;
-      ang_z = 0.0;
-    }
-    drive_robot(lin_x, ang_z);
+  if ((max_zone < min_threshold) || (max_zone > max_threshold)) {
+    lin_x = 0.0;
+    ang_z = 0.0;
   }
+  drive_robot(lin_x, ang_z);
 }
 
 int main(int argc, char** argv) {
@@ -82,7 +79,7 @@ int main(int argc, char** argv) {
   client = n.serviceClient<ball_chaser::DriveToTarget>("/ball_chaser/command_robot");
 
   ros::Subscriber sub1 = n.subscribe("/camera/rgb/image_raw", 10, process_image_callback);
-
+  ROS_INFO("Ready to follow white ball");
   ros::spin();
 
   return 0;
